9d4fcf3b10235b96c6692ce4090536b4d81846f4
Almog Sofer <45693218+TakNud@users.noreply.github.com>
2022-10-31 20:41:31 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,8 @@
 
 # VARABLES #
 machine=$1
-SECRET_KEY="/home/ec2-user/.ssh/id_rsa" 
+SECRET_KEY="~/.ssh/id_rsa" 
 JENKINS_PROJECT_FOLDER = /var/lib/jenkins/workspace/dev-Automation
 echo "deploying to $machine"
-echo "createing projenct directory"
-ssh -i "${SECRET_KEY}" ${machine} "mkdir -p /home/ec2-user/final-project"
-echo "yes"
-echo "copying..."
-scp -i "$(SECRET_KEY)" "${JENKINS_PROJECT_FOLDER}" "${machine}:/home/ec2-user/final-project"
-echo "yes"
+echo "createing directory and copy"
+scp -rp -i $SECRET_KEY -o StrictHostKeyChecking=no $JENKINS_PROJECT_FOLDER ec2-user@$machine:/home/ec2-user
