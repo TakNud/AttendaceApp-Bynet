fb9e4666ddd9fa774b949a057ebf94a0fd9d78fe
Almog Sofer <45693218+TakNud@users.noreply.github.com>
2022-11-06 16:21:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,13 @@ scp -o StrictHostKeyChecking=no -r $JENKINS_PROJECT_FOLDER ec2-user@$machine:/ho
 echo "COPIED to $machine"
 #scp -v -r -i $SECRET_KEY -o StrictHostKeyChecking=no $JENKINS_PROJECT_FOLDER ec2-user@$machine:/home/ec2-user/final
 #scp -i $SECRET_KEY $JENKINS_PROJECT_FOLDER ec2-user@$machine:~
-#if [$machine='test']
-#then
-#    curl test
-#fi
+docker-compose -f /home/ec2-user/final/dev-Automation/docker-compose.yml up -d
+if [$machine='test']
+then
+    echo 'Run Curl testing...'
+    if curl test > HTML_Output
+        then echo "Request was successful"
+        else echo "CURL Failed"
+    fi
+fi
     
