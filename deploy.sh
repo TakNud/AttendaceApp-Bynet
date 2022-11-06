3126e0c7cf83815262c01af1e162bfd9741946b6
Almog Sofer <45693218+TakNud@users.noreply.github.com>
2022-11-06 19:17:55 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,19 @@ SECRET_KEY="/var/lib/jenkins/.ssh/id_rsa"
 JENKINS_PROJECT_FOLDER="/var/lib/jenkins/workspace/dev-Automation/docker-compose.yml"
 echo "deploying to $machine"
 echo "createing directory and copy"
-#ssh -i $SECRET_KEY -o StrictHostKeyChecking=no $machine "mkdir -p /home/ec2-user/final"
 scp -o StrictHostKeyChecking=no -r $JENKINS_PROJECT_FOLDER ec2-user@$machine:/home/ec2-user/final
 echo "COPIED to $machine"
-#scp -v -r -i $SECRET_KEY -o StrictHostKeyChecking=no $JENKINS_PROJECT_FOLDER ec2-user@$machine:/home/ec2-user/final
-#scp -i $SECRET_KEY $JENKINS_PROJECT_FOLDER ec2-user@$machine:~
+
 docker-compose -f /home/ec2-user/final/dev-Automation/docker-compose.yml up -d
-if [$machine='test']
+if [$machine = "test"];
 then
     echo 'Run Curl testing...'
-    if curl test > HTML_Output
+    if [curl test > HTML_Output];
         then echo "Request was successful"
         else echo "CURL Failed"
     fi
+    docker login
+    docker pull almogso/attenapp:tagname 
     docker-compose -f /home/ec2-user/final/dev-Automation/docker-compose.yml down
     echo 'Test Docker Stopped !' 
 fi
