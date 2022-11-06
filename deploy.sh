d636ae8619da9d171cbc2080addddafaf25f7893
Almog Sofer <45693218+TakNud@users.noreply.github.com>
2022-11-06 19:22:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,16 @@ echo "deploying to $machine"
 echo "createing directory and copy"
 scp -o StrictHostKeyChecking=no -r $JENKINS_PROJECT_FOLDER ec2-user@$machine:/home/ec2-user/final
 echo "COPIED to $machine"
-
+docker login
+docker pull almogso/attenapp:tagname
 docker-compose -f /home/ec2-user/final/dev-Automation/docker-compose.yml up -d
-if [$machine = "test"];
+if [$machine == "test"];
 then
     echo 'Run Curl testing...'
     if [curl test > HTML_Output];
         then echo "Request was successful"
         else echo "CURL Failed"
-    fi
-    docker login
-    docker pull almogso/attenapp:tagname 
+    fi 
     docker-compose -f /home/ec2-user/final/dev-Automation/docker-compose.yml down
     echo 'Test Docker Stopped !' 
 fi
