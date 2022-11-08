f0000401c0cd278bcdb4996b637b4711bf1198c0
TakNud <45693218+TakNud@users.noreply.github.com>
2022-11-08 22:53:28 +0200

    change db connections and update app

--- attendance.py
+++ attendance.py
@@ -4,32 +4,36 @@
 
 import pandas as pd
 import os
+import shutil
 import glob
 import re
-#import pysftp
+import pysftp
 
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host="184.72.127.1",
+    host="127.0.0.1",
     user="root",
     password="root",
-    database="test_db"
+    database="test_db",
+    auth_plugin='mysql_native_password'
 )
+print(mydb)
 
 
-# def download_csvs():
-#   Hostname = "185.164.16.144"
-#   Username = "almogs"
-#   Password = "123456"
-#   localFilePath = "/app"
-#   cnopts = pysftp.CnOpts()
-#   cnopts.hostkeys = None
-#   with pysftp.Connection(host=Hostname, username=Username, password=Password, cnopts=cnopts) as sftp:
-#     print("Connection successfully established ... ")
-#     # Switch to a remote directory
-#     sftp.get_r("/var/tmp/csv_files/", localFilePath)
-#     print("done")
+def download_csvs():
+    Hostname = "185.164.16.144"
+    Username = "almogs"
+    Password = "123456"
+    localFilePath = "/uploads"
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    with pysftp.Connection(host=Hostname, username=Username, password=Password, cnopts=cnopts) as sftp:
+        print("Connection successfully established ... ")
+        # Switch to a remote directory
+        sftp.get_r("/var/tmp/csv_files/", localFilePath)
+        print("done")
+
 
 def att():
     # get the current path
@@ -59,3 +63,16 @@ def att():
 
     return df_new.to_csv("output.csv")
     #print("Merge All File in Directory Succeed! !")
+
+
+def removeFiles():
+    folder = '/path/to/folder'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
